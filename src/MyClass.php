e564d0d6cd08b705a82b48f22ba76a588ffa1c23
Plínio César Simões Fernandes <pliniocsfernandes@gmail.com>
2012-09-16 16:10:36 -0300

    Auto fixing code style with: php-cs-fixer.
    More here: https://github.com/fabpot/PHP-CS-Fixer

--- src/MyClass.php
+++ src/MyClass.php
@@ -2,16 +2,19 @@
 /**
  * @author Plinio Cesar Simoes Fernandes <pliniocsfernandes@gmail.com>
  */
-class MyClass {
+class MyClass
+{
     /**
      * @param $a
      *
      * @return int
      */
-    public function demo($a) {
-        if($a == 1) {
+    public function demo($a)
+    {
+        if ($a == 1) {
             return 1;
         }
+
         return 0;
     }
 }
