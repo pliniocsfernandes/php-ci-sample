1a7a4d7d7880f052c1ea609966a889b32860bda5
Plínio César Simões Fernandes <pliniocsfernandes@gmail.com>
2012-09-16 15:23:08 -0300

    Adding docblocks to test phpdox

--- src/MyClass.php
+++ src/MyClass.php
@@ -1,5 +1,13 @@
 <?php
+/**
+ * @author Plinio Cesar Simoes Fernandes <pliniocsfernandes@gmail.com>
+ */
 class MyClass {
+    /**
+     * @param $a
+     *
+     * @return int
+     */
     public function demo($a) {
         if($a == 1) {
             return 1;
